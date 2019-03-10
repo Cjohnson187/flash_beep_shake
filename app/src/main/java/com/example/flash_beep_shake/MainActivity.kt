96708f71b63ff6febe600a3e223878318cd7850c
chris <cjohn268@msudenver.edu>
2019-03-09 19:00:37 -0700

    finished, flash beep and shake all work

--- app/src/main/java/com/example/flash_beep_shake/MainActivity.kt
+++ app/src/main/java/com/example/flash_beep_shake/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.flash_beep_shake
 
+import android.annotation.TargetApi
 import android.content.Context
 import android.hardware.camera2.CameraManager
+import android.media.AudioManager
+import android.media.ToneGenerator
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -12,30 +15,60 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    @TargetApi(Build.VERSION_CODES.M)
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
 
         val vibratorService = getSystemService(Context.VIBRATOR_SERVICE ) as Vibrator
 
-        val buttonV:Button = findViewById(R.id.vibrate)
-        buttonV.setOnClickListener {
+        val buttonV:Button = this.findViewById(R.id.vibrate)
+            buttonV.setOnClickListener {
             vibratorService.vibrate(500)
         }
+
+        val buttonB:Button = this.findViewById(R.id.beep)
+        val tone = ToneGenerator(AudioManager.STREAM_MUSIC, 50)
+
+        buttonB.setOnClickListener {
+            tone.startTone(ToneGenerator.TONE_DTMF_C,500)
+        }
+
+
+
         val cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
-
-
-
-
 
         val buttonT:Button = findViewById(R.id.torch)
         val cameraId = cameraManager.getCameraIdList()[0]
-        val flashLightStatus =true
+        var flashLightStatus =false
+
+
+        buttonT.setOnClickListener {
+            if (flashLightStatus == false){
+                cameraManager.setTorchMode(cameraId, true)
+                flashLightStatus = true
+            }
+
+            else if (flashLightStatus == true) {
+                cameraManager.setTorchMode(cameraId, false)
+                flashLightStatus = false
+            }
+
         }
+
+
+
+
 
 
 
     }
+
+
+
 }
+
